0afed79e8909eee0766f9670c3637f0443e075d3
EGOIST <0x142857@gmail.com>
2018-12-05 00:02:24 +0800

    add a fancy title

--- api/home/index.js
+++ api/home/index.js
@@ -8,6 +8,8 @@ module.exports = handleError(async (req, res) => {
   )
   res.setHeader('content-type', 'text/html')
   res.end(`
+  <meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Version Badge</title>
   <link rel="stylesheet" href="https://unpkg.com/github-markdown-css@2.10.0/github-markdown.css">
   <style>
     .markdown-body {
